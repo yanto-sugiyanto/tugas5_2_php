f6e610553d229e8438e2a10d00eabea6a86e82c3
yanto-sugiyanto <71365660+yanto-sugiyanto@users.noreply.github.com>
2020-10-08 10:50:46 +0700

    Add files via upload

---
+++ tugas5_2.php
@@ -0,0 +1,27 @@
+
+  <h1>Operator String</h1>
+    <?php
+ echo "
+<table border=1>
+    <tr>
+        <td>Input 1</td>
+        <td>Input 2</td>
+        <td>hasil</td>
+        
+    </tr>
+   
+    <tr>
+        <td>"."Ayo" ."</td>
+        <td>"."Belajar " ."</td>
+        <td>"."Ayo" ."</td>
+       
+    </tr>
+    <tr>
+        <td>"."Belajar" ."</td>
+        <td>"."Niomic " ."</td>
+        <td>"."Bersama" ."</td>
+       
+    </tr>
+    </table>";
+    printf("<br>Gabungan :"."Ayo"."Belajar"."Bersama"."Niomic :)");
+?>
